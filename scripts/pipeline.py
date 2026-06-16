cd45a417a182f3066fda50195edd66cdda4b9825
ryantangmj <ryantangmj@gmail.com>
2026-06-16 20:39:46 +0800

    removed backfilling from pipeline

--- scripts/pipeline.py
+++ scripts/pipeline.py
@@ -152,10 +152,6 @@ def upload_chroma():
 if __name__ == "__main__":
     download_chroma()
 
-    # One-time backfill: add day_of_week to chunks that predate the field
-    from chunk_sermons import backfill_day_of_week
-    backfill_day_of_week(COLLECTION_SERMONS, CHROMA_DB_PATH)
-
     already_embedded = get_embedded_filenames()
     new_files = scrape_new(already_embedded)
 
